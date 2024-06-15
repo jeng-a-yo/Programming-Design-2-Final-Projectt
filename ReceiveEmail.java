661ea983a5548ef713ee9e947cf46eb6b5d255b1
AlbertWeirdo <fualbert20030507@gmail.com>
2024-06-15 14:09:02 +0800

    Update ReceiveEmail.java

--- ReceiveEmail.java
+++ ReceiveEmail.java
@@ -1,5 +1,19 @@
 package Programming_Design_2_Final_Project;
 
+import java.util.HashMap;
+import java.util.Properties;
+import javax.mail.Address;
+import javax.mail.Folder;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Multipart;
+import javax.mail.NoSuchProviderException;
+import javax.mail.Part;
+import javax.mail.Session;
+import javax.mail.Store;
+import javax.mail.internet.InternetAddress;
+
+
 public class ReceiveEmail {
     
 }
