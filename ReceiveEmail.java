270a458466b2f1510d47bb127243a6a28d345ae7
AlbertWeirdo <fualbert20030507@gmail.com>
2024-06-16 19:47:56 +0800

    Update ReceiveEmail.java

--- ReceiveEmail.java
+++ ReceiveEmail.java
@@ -1,5 +1,7 @@
 package Programming_Design_2_Final_Project;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Properties;
 import javax.mail.Address;
@@ -27,8 +29,60 @@ class FetchEmail {
     // private String storeType = "pop3";
     private String user = "albert920507@gmail.com";
     private String password = "rdbdkmtwafxakdpn";
+    private ArrayList<String> tokens = new ArrayList<>();
 
-    public FetchEmail() {
+    public FetchEmail(ArrayList<String> tokens, HashMap<String, Integer> attendanceRecord) {
+        this.tokens = tokens;
+        this.attendanceRecord = attendanceRecord;
+    }
+
+    public void firstCall() {
+        Properties properties = new Properties();
+        properties.put("mail.store.protocol", "pop3");
+        properties.put("mail.pop3.host", pop3Host);
+        properties.put("mail.pop3.port", "995");
+        properties.put("mail.pop3.starttls.enable", "true");
+        Session emailSession = Session.getDefaultInstance(properties);
+
+        try {
+            Store store = emailSession.getStore("pop3s");
+            store.connect(pop3Host, user, password);
+            Folder emailFolder = store.getFolder("INBOX");
+            emailFolder.open(Folder.READ_ONLY);
+            Message[] messages = emailFolder.getMessages();
+            System.out.println("messages.length---" + messages.length);
+
+            // String passwordProf = "niuewfnw"; // change accordingly
+            // System.out.println("Correct Password: " + passwordProf);
+            System.out.println("============================");
+
+            for (int i = 0; i < messages.length; i++) {
+                passwordStd = new String();
+
+                // first record (attend on time)
+                verifyPassword(messages[i]);
+
+            }
+
+            System.out.println("============================");
+            for (String s : attendanceRecord.keySet()) {
+                System.out.println("StudentID: " + s + ",Status: " +
+                        attendanceRecord.get(s));
+            }
+
+            emailFolder.close(false);
+            store.close();
+
+        } catch (NoSuchProviderException e) {
+            e.printStackTrace();
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void secondCall() {
         Properties properties = new Properties();
         properties.put("mail.store.protocol", "pop3");
         properties.put("mail.pop3.host", pop3Host);
@@ -51,16 +105,14 @@ class FetchEmail {
             for (int i = 0; i < messages.length; i++) {
                 passwordStd = new String();
 
-                // first record (attend on time)
-                verifyPassword(messages[i], passwordProf);
-
                 // second record (late to class)
-                // recordLate(messages[i], passwordProf);
+                recordLate(messages[i]);
             }
 
             System.out.println("============================");
             for (String s : attendanceRecord.keySet()) {
-                System.out.println("StudentID: " + s + ",Status: " + attendanceRecord.get(s));
+                System.out.println("StudentID: " + s + ",Status: " +
+                        attendanceRecord.get(s));
             }
 
             emailFolder.close(false);
@@ -75,7 +127,7 @@ class FetchEmail {
         }
     }
 
-    public void verifyPassword(Part p, String passwordProf) throws Exception {
+    public void verifyPassword(Part p) throws Exception {
         String studentID = new String();
 
         if (p instanceof Message) {
@@ -87,18 +139,32 @@ class FetchEmail {
             System.out.print("studentID:" + studentID);
             System.out.print(",passwordStd: [" + passwordStd.trim() + "]\n");
 
-            if (passwordStd.trim().equals(passwordProf)) {
-                System.out.println("PASSWORD MATCH!!");
+            if (tokens.contains(passwordStd.trim())) {
+                System.out.println("PASSWORD MATCH");
+                attendanceRecord.remove(studentID);
                 attendanceRecord.put(studentID, 1);
+                tokens.remove(passwordStd.trim());
+
             } else {
-                System.out.println("PASSWORD DIDN'T MATCH!!");
+                System.out.println("PASSWORD DIDN'T MATCH");
+                attendanceRecord.remove(studentID);
                 attendanceRecord.put(studentID, -1);
             }
+
+            // if (passwordStd.trim().equals(passwordProf)) {
+            // System.out.println("PASSWORD MATCH!!");
+            // attendanceRecord.remove(studentID);
+            // attendanceRecord.put(studentID, 1);
+            // } else {
+            // System.out.println("PASSWORD DIDN'T MATCH!!");
+            // attendanceRecord.remove(studentID);
+            // attendanceRecord.put(studentID, -1);
+            // }
         }
 
     }
 
-    public void recordLate(Part p, String passwordProf) throws Exception {
+    public void recordLate(Part p) throws Exception {
         String studentID = new String();
 
         if (p instanceof Message) {
@@ -111,19 +177,27 @@ class FetchEmail {
             System.out.print("studentID:" + studentID);
             System.out.print(",passwordStd: [" + passwordStd.trim() + "]\n");
 
-            if (passwordStd.trim().equals(passwordProf)) {
+            if (tokens.contains(passwordStd.trim())) {
                 System.out.println("PASSWORD MATCH");
-                if (attendanceRecord.keySet().contains(studentID)) {
-                    attendanceRecord.remove(studentID);
-                    attendanceRecord.put(studentID, 2);
-                } else {
-                    attendanceRecord.put(studentID, 2);
-                }
-
+                attendanceRecord.remove(studentID);
+                attendanceRecord.put(studentID, 2);
+                tokens.remove(passwordStd.trim());
             } else {
-                System.out.println("PASSWORD DOESN'T MATCH");
+                System.out.println("PASSWORD DIDN'T MATCH");
+                attendanceRecord.remove(studentID);
                 attendanceRecord.put(studentID, -1);
             }
+
+            // if (passwordStd.trim().equals(passwordProf)) {
+            // System.out.println("PASSWORD MATCH");
+            // attendanceRecord.remove(studentID);
+            // attendanceRecord.put(studentID, 2);
+
+            // } else {
+            // System.out.println("PASSWORD DOESN'T MATCH");
+            // attendanceRecord.remove(studentID);
+            // attendanceRecord.put(studentID, -1);
+            // }
         }
 
     }
